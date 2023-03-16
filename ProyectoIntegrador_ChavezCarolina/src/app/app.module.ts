ef3d3941e754d81b767971402a3f855b4e8d37b0
NinaChavezz <chavez.carolinaok@gmail.com>
2023-03-16 15:21:38 -0300

    AlgComp

--- ProyectoIntegrador_ChavezCarolina/src/app/app.module.ts
+++ ProyectoIntegrador_ChavezCarolina/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
