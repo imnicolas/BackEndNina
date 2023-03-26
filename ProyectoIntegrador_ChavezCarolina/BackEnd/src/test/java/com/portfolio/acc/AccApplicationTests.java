dcd3eb9bd314504a7ea376db39130a56f6529b33
NinaChavezz <chavez.carolinaok@gmail.com>
2023-03-26 15:17:35 -0300

    CreacionBackEnd

---
+++ ProyectoIntegrador_ChavezCarolina/BackEnd/src/test/java/com/portfolio/acc/AccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.acc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
